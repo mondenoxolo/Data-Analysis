7cfc61c77bdb878ac5ef88be599d82644fc6c088
noxolo <mondenoxolo@gmail.com>
2025-03-03 15:26:24 +0200

    Uploading initial data to database

--- API interactions.py
+++ API interactions.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import sqlite3
 
 
 def extracting_from_api():
@@ -22,19 +23,54 @@ def clean_json():
     with open("gdp_data.json", "r") as file:
         data = json.load(file)
         
-    cleaned_data = [{"year": entry["date"], "gpd": entry["value"]}
+    cleaned_data = [{"year": entry["date"], "gdp": entry["value"]}
                     for entry in data[1] if entry["value"] is not None]
     
     with open("cleaned_gdp_data.json", "w") as file:
         json.dump(cleaned_data, file, indent=4)
         
     print("Data was cleaned successfully")
+    return cleaned_data
+    
+def load_to_db(cleaned_data):
+    
+    conn = sqlite3.connect("finance.db")
+    cursor =  conn.cursor()
+    
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS gdp_data (
+                       year INTEGER PRIMARY KEY,
+                       gdp REAL
+                   )
+                   """)
+    
+    for entry in cleaned_data:
+        cursor.execute("""
+                       INSERT OR REPLACE INTO gdp_data (year, gdp) VALUES (?, ?)
+                       """, (entry["year"], entry["gdp"]))
+        
+    conn.commit()
+    conn.close()
     
     
-    
-    
+import sqlite3
+
+def view_db():
+    conn = sqlite3.connect("finance.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM gdp_data")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)  
+
+    conn.close()
+
     
         
 if __name__ == "__main__":
     extracting_from_api()
-    clean_json()
+    cleaned = clean_json()
+    load_to_db(cleaned)
+    view_db()
